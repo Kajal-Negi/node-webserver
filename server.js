b82ec639fe1c1a49c0b7ec3e80fba18e27a39009
saurabh <kajalnegi619@gmail.com>
2019-06-15 16:18:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const fs=require('fs');
+const express= require('express');
+const hbs= require('hbs');
+var app=express('web server/public');
+
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+ '/views/partials');
+app.use(express.static(__dirname+ '/public'));
+
+app.use((req,res,next)=>{ //middleware function
+	var now=new Date().toString();
+	var log=`${now}:${req.method}:${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log',log+ '\n',(err)=>{
+		if(err){console.log('Unable to append to serverlog');}
+	});
+	next();
+});
+
+app.use((req,res,next)=>{
+	res.render('maintainence.hbs');
+	
+});
+
+hbs.registerHelper('getCurrentYear',()=>{
+	return new Date().getFullYear()
+});
+hbs.registerHelper('scream',(text)=>{
+	return text.toUpperCase();
+});
+
+
+app.get('/',(req,response)=>{
+	//response.send('<h1>HELLO EXPRESS</h1>');
+    response.render('home.hbs',{
+	pageTitle:'Home Page',
+	message:'WElcome to the homepage',
+
+   
+});
+});
+
+app.get('/about',(req,response)=>{
+	response.render('about.hbs',{
+		pageTitle:'About Page',
+
+	});
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		errormessage:'Unable to handle request'
+		});
+});
+app.listen(3000,()=>{
+	console.log('Server is up on port:3000')
+});
